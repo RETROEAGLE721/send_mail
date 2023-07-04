182300ed8c7d82dfe53eca14365be588e35efc12
RETROEAGLE721 <137479671+RETROEAGLE721@users.noreply.github.com>
2023-07-04 19:43:26 +0530

    Update urls.py

--- send_mail/urls.py
+++ send_mail/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from . import backend
+from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls,name="dai"),
-    path('', backend.load_frontend,name=""),
+    path('admin/', admin.site.urls, name="dai"),
     path('check/', include('send_Email.urls')),
 ]
-
