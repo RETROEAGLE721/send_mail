9e9f182e5b71421d52e238688da60bcdb2984376
RETROEAGLE721 <137479671+RETROEAGLE721@users.noreply.github.com>
2023-07-07 08:10:13 +0530

    Update urls.py

--- send_mail/urls.py
+++ send_mail/urls.py
@@ -17,10 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import backend
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls, name="dai"),
     path('', backend.load_frontend, name=""),
     path('check/', include('send_Email.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
